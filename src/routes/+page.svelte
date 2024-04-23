999253eef216b200c7b8cab14caa2745765fe54f
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2024-04-23 20:09:46 +0200

    streamlined bootstrapping process

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
 	import UserCard from '$lib/components/UserCard.svelte';
