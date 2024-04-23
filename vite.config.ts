f33614a1e4cdc87693a4bdd5d7bac3d3a9d19f4b
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2024-04-23 20:42:42 +0200

    add unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import UnoCSS from 'unocss/vite'
+import extractorSvelte from '@unocss/extractor-svelte'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		UnoCSS({
+			extractors: [
+				extractorSvelte(),
+			],
+		}),
+		sveltekit()
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
