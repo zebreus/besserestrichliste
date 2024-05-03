8d0a7840ff7d0ff910269ccf0cae0b04f8914935
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2024-05-04 01:25:29 +0200

    basic transaction button styling

---
+++ src/lib/components/users/PriceButton.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let amount: 30 | 50 | 100 | 130 | 150 | 250;
+    export let type: 'deposit' | 'withdraw' = 'deposit';
+    const textBlack = type === 'withdraw' && (
+        amount === 30 || amount === 100 || amount === 130
+    );
+</script>
+
+<button
+    class="p-4 text-2xl font-bold border-3"
+    class:text-black={textBlack}
+    class:bg-white={type === 'withdraw' && amount === 30}
+    class:bg-blue-900={type === 'withdraw' && amount === 50}
+    class:bg-amber={type === 'withdraw' && amount === 100}
+    class:bg-green={type === 'withdraw' && amount === 130}
+    class:bg-red-950={type === 'withdraw' && amount > 130}
+    class:border-red={type === 'withdraw'}
+    class:border-green={type === 'deposit'}
+    class:bg-green-950={type === 'deposit'}
+    name="amount"
+    value="{amount}">
+    {type === 'deposit' ? '+' : '-'}{new Intl.NumberFormat(undefined, { style: 'currency', currency: 'EUR' }).format(amount / 100)}
+    </button>
--- src/routes/users/[id]/+page.svelte
+++ src/routes/users/[id]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Container from '$lib/components/Container.svelte';
+	import PriceButton from '$lib/components/users/PriceButton.svelte';
 	import TransactionTable from '$lib/components/users/TransactionTable.svelte';
 	import type { PageServerData } from './$types';
 
@@ -10,7 +11,6 @@
 	<div class="bg-neutral-700 text-xl font-bold">
 		<Container class="flex-1 flex justify-between">
 			<h1 class="p-2">{data.user.name}</h1>
-
 			<span
 				class="p-2"
 				class:bg-green-950={data.user.balance >= 0}
@@ -87,6 +87,34 @@
 	</div>
 
 	<main>
+		<Container class="grid grid-cols-3 mt-4 gap-4">
+			<form method="POST" action="?/deposit" class="grid grid-cols-3 gap-2">
+				<PriceButton amount={30} />
+				<PriceButton amount={50} />
+				<PriceButton amount={100} />
+				<PriceButton amount={130} />
+				<PriceButton amount={150} />
+				<PriceButton amount={250} />
+			</form>
+			<form method="POST" class="bg-neutral-800 p-4 gap-4 flex flex-col justify-between">
+				<label class="flex flex-col justify-center items-center gap-4">
+					<span class="font-bold text-xl">Amount</span>
+					<input class="p-2  text-xl bg-neutral-950 text-white border-2 text-white" name="amount" type="number" />
+				</label>
+				<div class="flex justify-center gap-8">
+					<button class="p-4 border-green bg-green-950 text-lg font-bold border-3" formaction="?/deposit">Deposit</button>
+					<button class="p-4 border-red bg-red-950 text-lg font-bold border-3" formaction="?/withdraw">Withdraw</button>	
+				</div>
+			</form>
+			<form method="POST" action="?/deposit" class="grid grid-cols-3 gap-2">
+				<PriceButton amount={30} type="withdraw" />
+				<PriceButton amount={50} type="withdraw" />
+				<PriceButton amount={100} type="withdraw" />
+				<PriceButton amount={130} type="withdraw" />
+				<PriceButton amount={150} type="withdraw" />
+				<PriceButton amount={250} type="withdraw" />
+			</form>
+		</Container>
 		<Container class="grid lg:grid-cols-2 my-4	gap-4">
 			<TransactionTable transacitons={data.user.initiatorIn} title="Initiated transactions" />
 			<TransactionTable transacitons={data.user.recipientIn} title="Received transactions" />
