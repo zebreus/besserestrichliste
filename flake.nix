8f27ed0b40a9fc2d39226833c1db2a4b6d76c8f7
Zebreus <lennart@zebre.us>
2024-05-11 14:50:21 +0200

    Move nixos module into separate file

--- flake.nix
+++ flake.nix
@@ -140,119 +140,16 @@
               '';
             };
 
-            nixosModules.besserestrichliste = ({ config, lib, pkgs, utils, ... }:
-
-              let
-                cfg = config.services.besserestrichliste;
-              in
-              {
-                options = {
-                  services.besserestrichliste = {
-                    enable = lib.mkEnableOption "Enable the besserestrichliste service.";
-
-                    host = lib.mkOption {
-                      type = lib.types.str;
-                      default = "localhost";
-                      example = "192.168.22.22";
-                      description = lib.mdDoc "Address to serve on.";
-                    };
-
-                    port = lib.mkOption {
-                      type = lib.types.int;
-                      default = 3000;
-                      example = 3000;
-                      description = lib.mdDoc "Port to serve on.";
-                    };
-
-                    origin = lib.mkOption {
-                      type = lib.types.str;
-                      default = "http://localhost:3000";
-                      example = "https://example.com";
-                      description = lib.mdDoc "Origin for CORS and stuff.";
-                    };
-
-                    package = lib.mkOption {
-                      type = lib.types.package;
-                      description = lib.mdDoc "besserestrichliste package used for the service.";
-                      default = packages.besserestrichliste;
-                      defaultText = lib.literalExpression "packages.besserestrichliste";
-                    };
-                  };
-                };
-
-                config = lib.mkIf cfg.enable (
-                  let
-                    dataDirectory = "/var/lib/besserestrichliste";
-                    databaseFile = "${dataDirectory}/besserestrichliste.db";
-                  in
-                  {
-                    users.users.besserestrichliste = {
-                      isSystemUser = true;
-                      createHome = true;
-                      home = dataDirectory;
-                      group = "besserestrichliste";
-                    };
-                    users.groups.besserestrichliste = { };
-
-                    systemd.services."besserestrichliste-init" = {
-                      description = "make sure the besserestrichliste database exists and the schema is up to date";
-                      serviceConfig = { Type = "oneshot"; };
-                      environment = {
-                        PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
-                        PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
-                        PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
-                        PRISMA_INTROSPECTION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/introspection-engine";
-                        PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
-                        DATABASE_URL = "file:${databaseFile}";
-                      };
-                      script = ''
-                        ${lib.getExe (pkgs.writeShellApplication {
-                          name = "besserestrichliste-init";
-                          runtimeInputs = with pkgs; [  nodePackages.prisma  ];
-                          text = ''
-                            cd ${cfg.package}
-                            export PATH="$PATH:${cfg.package}/node_modules/.bin"
-                            DB_CREATE=$(test -f "${databaseFile}" && echo false || echo true)
-                            prisma migrate deploy
-                            if $DB_CREATE; then
-                              # Seed if the db was just created
-                              prisma db seed
-                            fi
-                          '';
-                        })}
-                      '';
-                    };
-
-                    systemd.services."besserestrichliste" = {
-                      serviceConfig = {
-                        Type = "simple";
-                        User = "besserestrichliste";
-                        Group = "besserestrichliste";
-                        Restart = "on-failure";
-                        RestartSec = "30s";
-                        ExecStart = "${lib.getExe pkgs.nodejs} ${cfg.package}/build";
-                      };
-                      wantedBy = [ "multi-user.target" ];
-                      after = [ "besserestrichliste-init.service" ];
-
-                      description = "besserestrichliste UI and server";
-
-                      environment = {
-                        DATABASE_URL = "file:${databaseFile}";
-                        HOST = "${cfg.host}";
-                        PORT = "${builtins.toString cfg.port}";
-                        ORIGIN = "${cfg.origin}";
-                        NODE_ENV = "production";
-                      };
-
-                      documentation = [
-                        "https://github.com/zebreus/besserestrichliste"
-                      ];
-                    };
-                  }
-                );
-              }
-            );
+            nixosModules.besserestrichliste = {
+              # Insert besserestrichliste into nixpkgs
+              nixpkgs.overlays = [
+                (final: prev: {
+                  besserestrichliste = packages.besserestrichliste;
+                })
+              ];
+              # Load nixpkgs module
+              imports = [ ./module.nix ];
+            };
 
             formatter = pkgs.nixfmt-rfc-style;
           }
