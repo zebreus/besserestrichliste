3283aa65e6ef2393c59c75cd921342baf186ad86
Zebreus <zebreus@zebre.us>
2025-05-22 23:17:26 +0200

    Add secret poem page

---
+++ src/routes/secret/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>Secret Poem</title>
+</svelte:head>
+
+<div class="text-column">
+	<h1>A Poem for Nojus</h1>
+	<p>
+		Hidden away from prying eyes,<br />
+		This little verse is our surprise.<br />
+		Roses are red and violets gleam,<br />
+		Nojus, you make our Svelte app dream.
+	</p>
+</div>
