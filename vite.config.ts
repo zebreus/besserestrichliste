a52cb81f6fe979131724e5449c1cd23b47b0e9a8
Zebreus <zebreus@zebre.us>
2025-06-09 17:52:44 +0200

    Add svelte testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import path from 'node:path';
 import UnoCSS from 'unocss/vite';
 import extractorSvelte from '@unocss/extractor-svelte';
 
@@ -11,11 +10,6 @@ export default defineConfig({
 		}),
 		sveltekit()
 	],
-	resolve: {
-		alias: {
-			'@testing-library/svelte': path.resolve('./src/test-utils/testing-library-svelte.ts')
-		}
-	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		environment: 'jsdom'
